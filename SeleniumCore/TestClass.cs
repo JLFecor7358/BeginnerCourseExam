7f2f5e6bd03df7d2489f587d2abd5b3f15b0b3cd
Antwan Maddox <antwan.maddox@agilethought.com>
2022-02-11 11:36:10 -0500

    added first time

---
+++ SeleniumCore/TestClass.cs
@@ -0,0 +1,39 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SeleniumCore
+{
+    [TestFixture]
+    public class TestClass
+    {
+        IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            _driver.Navigate().GoToUrl("http://www.google.com");
+            Assert.IsTrue(_driver.Title.Contains("Google"));
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+
+        }
+    }
+}
