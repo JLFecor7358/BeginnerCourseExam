7fe25dc9a2ca3764e5585e89dab875558294f82d
Jose Luis Fecor Perez <fecor06@hotmail.com>
2024-06-21 04:16:15 -0600

    Automation Beginner Course Exam Resolved

--- SeleniumCore/TestClass.cs
+++ SeleniumCore/TestClass.cs
@@ -3,6 +3,8 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -20,20 +22,54 @@ namespace SeleniumCore
             _driver.Manage().Window.Maximize();
 
         }
-
         [Test]
-        public void TestMethod()
+        [Category("Smoke Test")]
+        public void LoginSauceDemo()
         {
-            _driver.Navigate().GoToUrl("http://www.google.com");
-            Assert.IsTrue(_driver.Title.Contains("Google"));
-            
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            _driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            _driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            _driver.FindElement(By.Id("login-button")).Submit();
+            Assert.That(_driver.FindElement(By.Id("shopping_cart_container")).Displayed);
+        }
+        [Test]
+        [Category("Smoke Test")]
+        public void AddToCart()
+        {
+            LoginSauceDemo();
+            _driver.FindElement(By.Name("add-to-cart-sauce-labs-backpack")).Click();
+            _driver.FindElement(By.Id("shopping_cart_container")).Click();
+            Assert.That((_driver.FindElement(By.Id("item_4_title_link")).Text),Is.EqualTo("Sauce Labs Backpack"));
+        }
+        [Test]
+        [Category("Smoke Test")]
+        public void CartCheckout()
+        {
+            AddToCart();
+            _driver.FindElement(By.Id("checkout")).Click();
+            _driver.FindElement(By.Id("first-name")).SendKeys("Julio");
+            _driver.FindElement(By.Id("last-name")).SendKeys("Lopez");
+            _driver.FindElement(By.Id("postal-code")).SendKeys("12345");
+            _driver.FindElement(By.Id("continue")).Submit();
+            _driver.FindElement(By.Id("finish")).Click();
+            Assert.That(_driver.FindElement(By.Id("back-to-products")).Displayed);
+        }
+        [Test]
+        [Category("Smoke Test")]
+        public void LogoutSauceDemo()
+        {
+            LoginSauceDemo();
+            _driver.FindElement(By.Id("react-burger-menu-btn")).Click();
+            WebDriverWait wait = new WebDriverWait(_driver,TimeSpan.FromSeconds(5));
+            wait.Until(d => _driver.FindElement(By.Id("logout_sidebar_link")).Displayed);
+            _driver.FindElement(By.Id("logout_sidebar_link")).Click();
+            Assert.That(_driver.FindElement(By.Id("login-button")).Displayed);
         }
 
         [TearDown]
         public void TearDown()
         {
             _driver.Quit();
-
         }
     }
 }
